3eed6a38763bf56638ab26d262392d751eb5afb7
alex4401 <rylatgl@gmail.com>
2022-10-31 08:45:29 +0100

    refactor: restructure the popup script

--- Generic/js/popup.js
+++ Generic/js/popup.js
@@ -1,5 +1,5 @@
 const storage = window.storage || chrome.storage,
-	defaults = {
+	userDefaults = {
 		isRedirectDisabled: false,
 		searchMode: 'rewrite',
 		disabledWikis: []
@@ -10,120 +10,133 @@ const storage = window.storage || chrome.storage,
 		{ id: 'noita', name: 'Noita' },
 		{ id: 'temtem', name: 'Temtem' },
 		{ id: 'terraria', name: 'Terraria' },
+		{ id: 'calamitymod', name: 'Calamity Mod', uiClass: 'wiki-indent' },
+		{ id: 'thoriummod', name: 'Thorium Mod', uiClass: 'wiki-indent' },
 		{ id: 'undermine', name: 'UnderMine' },
+	];
+
+
+const RTW = {
+	$container: document.getElementById( 'wikis' ),
+	settingsIds: [],
+	updateCallbacks: [
+		( settings => {
+			RTW.settingsCache = settings;
+			RTW.disabledWikis = this.getCurrentSettingValue( 'disabledWikis' );
+		} )
 	],
-	keys = [],
-	updateCallbacks = [],
-	$container = document.getElementById( 'wikis' );
-let disabledWikis = [];
+	disabledWikis: [],
+	settingsCache: {},
 
 
-updateCallbacks.push( settings => {
-	disabledWikis = settings.disabledWikis || defaults.disabledWikis;
-} );
+	getCurrentSettingValue( key ) {
+		return this.settingsCache && this.settingsCache[key] || userDefaults[key];
+	},
 
 
-function bindOptionCheckboxToggle( checkbox ) {
-	const settingId = checkbox.getAttribute( 'data-setting-id' ),
-		invertValue = checkbox.getAttribute( 'data-invert' ) === 'true',
-		arrayValue = checkbox.getAttribute( 'data-array-value' );
-	keys.push( settingId );
-	updateCallbacks.push( settings => {
-		let rawValue = settings[settingId] == null ? defaults[settingId] : settings[settingId];
-		if ( settingId === 'disabledWikis' && arrayValue ) {
-			rawValue = rawValue.indexOf( arrayValue ) >= 0;
-		}
-		checkbox.checked = invertValue ? !rawValue : rawValue;
-	} );
-	checkbox.addEventListener( 'change', () => {
-		const obj = {};
-		let value = null;
-
-		if ( settingId === 'disabledWikis' && arrayValue ) {
-			let add = ( invertValue && !checkbox.checked ) || ( !invertValue && checkbox.checked );
-			if ( add ) {
-				disabledWikis.push( arrayValue );
-			} else {
-				disabledWikis = disabledWikis.filter( x => x != arrayValue );
+	bindCheckboxToOption( checkbox ) {
+		const settingId = checkbox.getAttribute( 'data-setting-id' ),
+			invertValue = checkbox.getAttribute( 'data-invert' ) === 'true',
+			arrayValue = checkbox.getAttribute( 'data-array-value' );
+		this.settingsIds.push( settingId );
+		this.updateCallbacks.push( _ => {
+			let rawValue = this.getCurrentSettingValue(settingId);
+			if ( settingId === 'disabledWikis' && arrayValue ) {
+				rawValue = rawValue.indexOf( arrayValue ) >= 0;
 			}
-			value = disabledWikis;
-		} else {
-			value = invertValue ? !checkbox.checked : checkbox.checked;
-		}
-		
-		obj[settingId] = value;
-		storage.local.set( obj );
-		updateUI();
-	} );
-}
+			checkbox.checked = invertValue ? !rawValue : rawValue;
+		} );
 
-
-function bindOptionRadioToggle( radio ) {
-	const settingId = radio.getAttribute( 'data-setting-id' ),
-		value = radio.getAttribute( 'data-value' );
-	keys.push( settingId );
-	updateCallbacks.push( settings => {
-		radio.checked = value == ( settings[settingId] || defaults[settingId] );
-	} );
-	radio.addEventListener( 'change', () => {
-		if ( radio.checked ) {
+		checkbox.addEventListener( 'change', () => {
 			const obj = {};
+			let value = null;
+
+			if ( settingId === 'disabledWikis' && arrayValue ) {
+				let add = ( invertValue && !checkbox.checked ) || ( !invertValue && checkbox.checked );
+				if ( add ) {
+					this.disabledWikis.push( arrayValue );
+				} else {
+					this.disabledWikis = this.disabledWikis.filter( x => x != arrayValue );
+				}
+				value = this.disabledWikis;
+			} else {
+				value = invertValue ? !checkbox.checked : checkbox.checked;
+			}
+
 			obj[settingId] = value;
 			storage.local.set( obj );
-			updateUI();
+			this.update();
+		} );
+	},
+	
+
+	bindRadioToOption( radio ) {
+		const settingId = radio.getAttribute( 'data-setting-id' ),
+			value = radio.getAttribute( 'data-value' );
+		this.settingsIds.push( settingId );
+		updateCallbacks.push( _ => {
+			radio.checked = value == this.getCurrentSettingValue( settingId );
+		} );
+		radio.addEventListener( 'change', () => {
+			if ( radio.checked ) {
+				const obj = {};
+				obj[settingId] = value;
+				storage.local.set( obj );
+				this.update();
+			}
+		} );
+	},
+
+
+	addWikiEntry( info ) {
+		const $out = document.createElement( 'li' );
+		if ( info.uiClass ) {
+			$out.classList.add( info.uiClass );
 		}
-	} );
-}
+	
+		const $checkbox = document.createElement( 'input' );
+		$checkbox.setAttribute( 'id', 'wiki-cb-' + info.id );
+		$checkbox.setAttribute( 'type', 'checkbox' );
+		$checkbox.setAttribute( 'data-setting-id', 'disabledWikis' );
+		$checkbox.setAttribute( 'data-array-value', info.id );
+		$checkbox.setAttribute( 'data-invert', 'true' );
+		$out.appendChild( $checkbox );
+	
+		const $label = document.createElement( 'label' );
+		$label.setAttribute( 'for', 'wiki-cb-' + info.id );
+		$label.innerText = info.name;
+		$out.appendChild( $label );
+	
+		const $link = document.createElement( 'a' );
+		$link.setAttribute( 'href', `https://${info.id}.wiki.gg` );
+		$link.setAttribute( 'target', '_blank' );
+		$out.appendChild( $link );
+	
+		$container.appendChild( $out );
+	},
 
 
-function updateUI() {
-	storage.local.get( keys, result => {
-		for ( const callback of updateCallbacks ) {
-			callback( result || defaults );
-		}
-	} );
-}
+	update() {
+		storage.local.get( this.settingsIds, result => {
+			for ( const callback of this.updateCallbacks ) {
+				callback( result );
+			}
+		} );
+	}
+};
 
 
-function buildWikiEntry( info ) {
-	var $out = document.createElement( 'li' );
-
-	var $checkbox = document.createElement( 'input' );
-	$checkbox.setAttribute( 'id', 'wiki-cb-' + info.id );
-	$checkbox.setAttribute( 'type', 'checkbox' );
-	$checkbox.setAttribute( 'data-setting-id', 'disabledWikis' );
-	$checkbox.setAttribute( 'data-array-value', info.id );
-	$checkbox.setAttribute( 'data-invert', 'true' );
-	$out.appendChild( $checkbox );
-
-	var $label = document.createElement( 'label' );
-	$label.setAttribute( 'for', 'wiki-cb-' + info.id );
-	$label.innerText = info.name;
-	$out.appendChild( $label );
-
-	var $link = document.createElement( 'a' );
-	$link.setAttribute( 'href', `https://${info.id}.wiki.gg` );
-	$link.setAttribute( 'target', '_blank' );
-	$out.appendChild( $link );
-
-	$container.appendChild( $out );
-}
-
-
-function initialiseUI() {
+( function _initialiseUI() {
 	for ( const wiki of wikis ) {
-		buildWikiEntry( wiki );
+		RTW.addWikiEntry( wiki );
 	}
 
 	for ( const checkbox of document.querySelectorAll( 'input[type=checkbox][data-setting-id]' ) ) {
-		bindOptionCheckboxToggle( checkbox );
+		RTW.bindCheckboxToOption( checkbox );
 	}
 	for ( const radio of document.querySelectorAll( 'input[type=radio][data-setting-id]' ) ) {
-		bindOptionRadioToggle( radio );
+		RTW.bindRadioToOption( radio );
 	}
 
-	updateUI();
-}
-
-
-initialiseUI();
+	RTW.update();
+} )();
