cdb77f2c4b9a069d3891397674d58a29d9beb7ae
alex4401 <rylatgl@gmail.com>
2022-10-31 10:55:25 +0100

    refactor!: restructure the background script

--- Generic/js/background.js
+++ Generic/js/background.js
@@ -1,80 +1,89 @@
 'use strict';
 
-const wikis = [
+const storage = chrome && chrome.storage || window.storage,
+    wikis = [
     // TODO: share this list with other parts of the extension
     { id: 'ark', name: 'ARK: Survival Evolved' },
     { id: 'noita', name: 'Noita' },
     { id: 'temtem', name: 'Temtem' },
     { id: 'terraria', name: 'Terraria' },
+    { id: 'calamitymod', name: 'Calamity Mod' },
+    { id: 'thoriummod', name: 'Thorium Mod' },
     { id: 'undermine', name: 'UnderMine' },
 ];
 
-const domains = wikis.map( item => item.id ).join( '|' );
-const domainRegex = new RegExp( `^((${domains})\\.fandom|(${domains})\\.gamepedia|(${domains})-[a-z]+\\.gamepedia)\\.com$`, 'i' );
 
-let isAddonDisabled = false,
-    disabledWikis = [];
-const storage = chrome && chrome.storage || window.storage;
-
-
-function updateIcon() {
-    const icon = {
-        path: ( isAddonDisabled ? '/icons/32_black.png' : '/icons/32.png' )
-    };
-    if ( chrome && chrome.action && chrome.action.setIcon ) {
-        chrome.action.setIcon( icon );
-    } else {
-        chrome.browserAction.setIcon( icon );
-    }
-}
+const RTW = {
+    settings: {
+        disableRedirects: false,
+        disabledWikis: []
+    },
+    domainRegex: ( () => {
+        const domains = wikis.map( item => item.id ).join( '|' );
+        return new RegExp( `^((${domains})\\.fandom|(${domains})\\.gamepedia|(${domains})-[a-z]+\\.gamepedia)\\.com$`, 'i' );
+    } )(),
 
 
-// Redirect from Fandom onto the official wiki
-chrome.webNavigation.onBeforeNavigate.addListener( info => {
-    if ( isAddonDisabled ) {
-        return;
-    }
-
-    const url = new URL( info.url );
-
-    // Check if the URL matches our regex
-    if ( !domainRegex.test( url.host ) ) {
-        return;
-    }
-
-    const oldWikiDomain = url.host.split( '.' )[0].toLowerCase();
-    const domainParts = oldWikiDomain.split( '-' );
-    let wikiId = domainParts[0];
-    let newPath = url.pathname;
-
-    if ( disabledWikis.indexOf( wikiId ) >= 0 ) {
-        return;
-    }
-
-    if ( domainParts.length > 1 ) {
-        let languageCode = domainParts[1];
-        if ( languageCode == 'ptbr' ) {
-            languageCode = 'pt-br';
+    updateIcon() {
+        const icon = {
+            path: ( this.settings.disableRedirects ? '/icons/32_black.png' : '/icons/128.png' )
+        };
+        if ( chrome && chrome.action && chrome.action.setIcon ) {
+            chrome.action.setIcon( icon );
+        } else {
+            chrome.browserAction.setIcon( icon );
         }
-        newPath = `/${ languageCode }${ newPath }`;
+    },
+
+    
+    decideRedirect( info ) {
+        if ( this.disableRedirects ) {
+            return;
+        }
+
+        const url = new URL( info.url );
+
+        // Check if the URL matches our regex
+        if ( !this.domainRegex.test( url.host ) ) {
+            return;
+        }
+
+        const oldWikiDomain = url.host.split( '.' )[0].toLowerCase();
+        const domainParts = oldWikiDomain.split( '-' );
+        let wikiId = domainParts[0];
+        let newPath = url.pathname;
+        if ( this.settings.disabledWikis.indexOf( wikiId ) >= 0 ) {
+            return;
+        }
+
+        if ( domainParts.length > 1 ) {
+            let languageCode = domainParts[1];
+            if ( languageCode == 'ptbr' ) {
+                languageCode = 'pt-br';
+            }
+            newPath = `/${ languageCode }${ newPath }`;
+        }
+    
+        chrome.tabs.update( info.tabId, {
+            url: `https://${ wikiId }.wiki.gg${ newPath }`
+        } );
+    },
+
+    
+    mergeStorageChunk( chunk ) {
+        const oldIRD = this.settings.isRedirectDisabled;
+
+        for ( const key in chunk ) {
+            this.settings[key] = chunk[key];
+        }
+
+        if ( this.settings.isRedirectDisabled !== oldIRD ) {
+            this.updateIcon();
+        }
     }
-
-    chrome.tabs.update( info.tabId, {
-        url: `https://${ wikiId }.wiki.gg${ newPath }`
-    } );
-});
+};
 
 
-function updateIsEnabled() {
-    storage.local.get( [ 'isRedirectDisabled', 'disabledWikis' ], result => {
-        isAddonDisabled = result ? result.isRedirectDisabled : false;
-        disabledWikis = result && result.disabledWikis || [];
-        updateIcon();
-    } );
-}
-
-
-storage.onChanged.addListener( ( changes, _ ) => {
-    updateIsEnabled();
-} );
-updateIsEnabled();
+chrome.webNavigation.onBeforeNavigate.addListener( info => RTW.decideRedirect( info ) );
+storage.onChanged.addListener( ( changes, _ ) => RTW.mergeStorageChunk( changes ) );
+storage.local.get( [ 'isRedirectDisabled', 'disabledWikis' ], result => RTW.mergeStorageChunk( result ) )
