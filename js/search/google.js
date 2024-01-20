62756a3c11da9af4e949275c6e094e549b8e5712
alex4401 <rylatgl@gmail.com>
2024-01-20 13:14:54 +0100

    refactor: initial port of Google search module onto new architecture

--- js/search/google.js
+++ js/search/google.js
@@ -1,13 +1,16 @@
+/** @typedef {import( '../util.js' ).SiteRecord} SiteRecord */
 'use strict';
 
 
 import { getWikis } from '../util.js';
 import {
+    GenericSearchModule,
     prepareWikisInfo,
     invokeSearchModule,
     crawlUntilParentFound,
     awaitElement
 } from './baseSearch.js';
+import { constructRedirectBadge } from './components.js';
 
 
 const wikis = prepareWikisInfo( getWikis( false, true ), {
@@ -16,87 +19,11 @@ const wikis = prepareWikisInfo( getWikis( false, true ), {
 } );
 
 
-// Looks for a search result for a wiki.gg wiki
-function findNextOfficialWikiResult( wiki, oldElement ) {
-    for ( const node of document.querySelectorAll( wiki.search.goodSelector ) ) {
-        if ( node.compareDocumentPosition( oldElement ) & 0x02 ) {
-            return crawlUntilParentFound( node, '.g' );
-        }
-    }
-    return null;
-}
+const rewriteUtil = {
+    MARKER_ATTRIBUTE: 'data-ggr-processed',
 
 
-// Replaces a Fandom result with an official wiki result or a placeholder
-const filter = {
-    makePlaceholderElement( wiki ) {
-        const element = document.createElement( 'span' );
-        element.innerHTML = 'Result from ' + wiki.search.placeholderTitle + ' hidden by wiki.gg redirector';
-        element.style.paddingBottom = '1em';
-        element.style.display = 'inline-block';
-        element.style.color = '#5f6368';
-        return element;
-    },
-
-
-    run( wiki, linkElement ) {
-        // If no parent, skip - means we've already processed this
-        if ( linkElement.parentElement ) {
-            // Find result container
-            const oldElement = crawlUntilParentFound( linkElement, '.g, .xpd' );
-            if ( oldElement === null ) {
-                return;
-            }
-
-            // Verify that the top-level result is a link the same wiki
-            const topLevelLinkElement = oldElement.querySelector( 'a[data-jsarwt="1"], a[ping]' );
-            if ( topLevelLinkElement && !topLevelLinkElement.href.startsWith( `https://${wiki.oldId || wiki.id}.fandom.com` ) ) {
-                return;
-            }
-
-            if ( oldElement !== null ) {
-                // Find an official wiki result after this one
-                const officialResult = findNextOfficialWikiResult( wiki, oldElement );
-                if ( officialResult ) {
-                    // Move the official result before this one
-                    oldElement.parentNode.insertBefore( officialResult, oldElement );
-                } else {
-                    // Insert a placeholder before this result
-                    oldElement.parentNode.insertBefore( this.makePlaceholderElement( wiki ), oldElement );
-                }
-                // Delete this result
-                oldElement.remove();
-            }
-        }
-    }
-};
-
-
-// Rewrites a Fandom result to an official wiki link to help users switch
-const rewrite = {
-    MARKER_ATTRIBUTE: 'data-ark',
-    // TODO: do not hardcode any selectors!
-    SITE_NETWORK_TITLE_SELECTOR: 'span.VuuXrf',
-    TRANSLATE_SELECTOR: '.fl.iUh30',
-    MORE_FROM_NETWORK_SELECTOR: 'a.fl[href*="site:fandom.com"]',
-
-
-    makeBadgeElement( isMobile, isTopLevel ) {
-        const out = document.createElement( 'span' );
-        out.textContent = isTopLevel ? 'redirected' : 'some redirected';
-        out.style.backgroundColor = '#0002';
-        out.style.fontSize = '90%';
-        out.style.borderRadius = '4px';
-        out.style.padding = '1px 6px';
-        if ( !isMobile ) {
-            out.style.marginLeft = '4px';
-        }
-        out.style.opacity = '0.6';
-        return out;
-    },
-
-
-    rewriteLink( wiki, link ) {
+    doLink( wiki, link ) {
         if ( link.tagName.toLowerCase() === 'a' ) {
             if ( link.href.startsWith( '/url?' ) ) {
                 link.href = ( new URLSearchParams( link.href ) ).get( 'url' );
@@ -113,17 +40,17 @@ const rewrite = {
     },
 
 
-    rewriteText( wiki, text ) {
+    doTitleText( wiki, text ) {
         return text.replace( wiki.search.titlePattern, wiki.search.newTitle );
     },
 
 
-    rewriteH3( wiki, node ) {
+    doH3( wiki, node ) {
         for ( const child of node.childNodes ) {
             if ( child.textContent ) {
-                child.textContent = this.rewriteText( wiki, child.textContent );
+                child.textContent = this.doTitleText( wiki, child.textContent );
             } else {
-                this.rewriteH3( wiki, child );
+                this.doH3( wiki, child );
             }
         }
     },
@@ -136,75 +63,138 @@ const rewrite = {
 
     isLocked( element ) {
         return element.getAttribute( this.MARKER_ATTRIBUTE ) === '1';
-    },
-
-
-    run( wiki, linkElement ) {
-        if ( linkElement.parentElement ) {
-            // Find result container
-            const element = crawlUntilParentFound( linkElement, '.g, .xpd' );
-            if ( element === null ) {
-                return;
-            }
-
-            const oldDomain = `${wiki.oldId || wiki.id}.fandom.com`,
-                newDomain = `${wiki.id}.wiki.gg`;
-
-            // Verify that the top-level result is a link the same wiki
-            const isMobile = element.classList.contains( 'xpd' ),
-                topLevelLinkElement = !isMobile && element.querySelector( 'a[data-jsarwt="1"], a[ping]' ),
-                isTopLevel = isMobile || topLevelLinkElement && topLevelLinkElement.href.startsWith( `https://${oldDomain}` );
-
-            if ( element !== null ) {
-                const networkHeader = element.querySelector( this.SITE_NETWORK_TITLE_SELECTOR );
-                if ( networkHeader ) {
-                    networkHeader.innerText = 'wiki.gg';
-                    networkHeader.appendChild( this.makeBadgeElement( isMobile, isTopLevel ) );
-                    this.lock( networkHeader );
-                }
-
-                this.rewriteLink( wiki, linkElement );
-                // Rewrite title
-                for ( const h3 of element.getElementsByTagName( 'h3' ) ) {
-                    this.rewriteH3( wiki, h3 );
-                    // Insert a badge indicating the result was modified if we haven't done that already (check heading and
-                    // result group)
-                    if ( !networkHeader && !this.isLocked( element ) && !this.isLocked( h3 ) ) {
-                        const badgeElement = this.makeBadgeElement( isMobile, isTopLevel );
-                        h3.parentNode.parentNode.insertBefore( badgeElement, h3.parentNode.nextSibling );
-                    }
-                    // Tag heading and result group as ones we badged
-                    this.lock( element );
-                    this.lock( h3 );
-                }
-                // Rewrite URL element
-                if ( !isMobile ) {
-                    for ( const cite of element.getElementsByTagName( 'cite' ) ) {
-                        if ( cite.firstChild.textContent ) {
-                            cite.firstChild.textContent = cite.firstChild.textContent.replace( oldDomain, newDomain );
-                        }
-                    }
-                } else {
-                    const mobileBreadcrumb = element.querySelector( '.sCuL3 > div' );
-                    if ( mobileBreadcrumb ) {
-                        mobileBreadcrumb.textContent = mobileBreadcrumb.textContent.replace( oldDomain, newDomain );
-                    }
-                }
-                // Rewrite translate link
-                for ( const translate of element.querySelectorAll( this.TRANSLATE_SELECTOR ) ) {
-                    this.rewriteLink( wiki, translate );
-                }
-
-                // Look for "More results from" in this result group and switch them onto wiki.gg
-                for ( const moreResults of element.querySelectorAll( this.MORE_FROM_NETWORK_SELECTOR ) ) {
-                    moreResults.href = moreResults.href.replace( 'site:fandom.com', 'site:wiki.gg' )
-                        .replace( `site:${wiki.oldId || wiki.id}.fandom.com`, `site:${wiki.id}.wiki.gg` );
-                    moreResults.innerText = moreResults.innerText.replace( 'fandom.com', 'wiki.gg' );
-                }
-            }
-        }
     }
 };
+
+
+class GoogleSearchModule extends GenericSearchModule {
+    SITE_NETWORK_TITLE_SELECTOR = 'span.VuuXrf';
+    TRANSLATE_SELECTOR = '.fl.iUh30';
+    MORE_FROM_NETWORK_SELECTOR = 'a.fl[href*="site:fandom.com"]';
+
+
+    /**
+     * Finds a general result container for a given element, if any.
+     *
+     * @protected
+     * @param {HTMLElement} element Element to find result container for.
+     * @return {HTMLElement?}
+     */
+    resolveResultContainer( element ) {
+        return crawlUntilParentFound( element, '.g, .xpd' );
+    }
+
+
+    /**
+     * @protected
+     * @param {SiteRecord} wikiInfo
+     * @param {HTMLElement} boundaryElement
+     * @return {HTMLElement?}
+     */
+    findNearestGgResult( wikiInfo, boundaryElement ) {
+        for ( const node of document.querySelectorAll( wikiInfo.search.goodSelector ) ) {
+            if ( node.compareDocumentPosition( boundaryElement ) & 0x02 ) {
+                return crawlUntilParentFound( node, '.g' );
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * @private
+     * @param {SiteRecord} wikiInfo
+     * @param {HTMLElement} elementToCheck
+     * @return {boolean}
+     */
+    _isTopLevelResult( wikiInfo, elementToCheck ) {
+        const
+            oldUrl = `https://${wikiInfo.oldId || wikiInfo.id}.fandom.com`,
+            topLevelLinkElement = elementToCheck.querySelector( 'a[data-jsarwt="1"], a[ping]' );
+        return topLevelLinkElement && !topLevelLinkElement.href.startsWith( oldUrl );
+    }
+
+
+    /**
+     * @protected
+     * @param {SiteRecord} wikiInfo
+     * @param {HTMLElement} containerElement
+     * @param {HTMLElement} _foundLinkElement
+     */
+    async hideResult( wikiInfo, containerElement, _foundLinkElement ) {
+        if ( this._isTopLevelResult( wikiInfo, containerElement ) ) {
+            super.hideResult( wikiInfo, containerElement, _foundLinkElement );
+        }
+    }
+
+
+    /**
+     * @protected
+     * @param {SiteRecord} wikiInfo
+     * @param {HTMLElement} containerElement
+     * @param {HTMLElement} foundLinkElement
+     */
+    async replaceResult( wikiInfo, containerElement, foundLinkElement ) {
+        const oldDomain = `${wikiInfo.oldId || wikiInfo.id}.fandom.com`,
+            newDomain = `${wikiInfo.id}.wiki.gg`;
+        const isMobile = containerElement.classList.contains( 'xpd' ),
+            isTopLevel = isMobile || this._isTopLevelResult( wikiInfo, containerElement );
+
+        // Rewrite title
+        for ( const h3 of containerElement.getElementsByTagName( 'h3' ) ) {
+            rewriteUtil.doH3( wikiInfo, h3 );
+            // Insert a badge indicating the result was modified if we haven't done that already (check heading and
+            // result group)
+            if ( !networkHeader && !rewriteUtil.isLocked( containerElement ) && !rewriteUtil.isLocked( h3 ) ) {
+                const badgeElement = constructRedirectBadge( {
+                    isGoogleMobile: isMobile,
+                    allMoved: isTopLevel
+                } );
+                h3.parentNode.parentNode.insertBefore( badgeElement, h3.parentNode.nextSibling );
+            }
+            // Tag heading and result group as ones we badged
+            rewriteUtil.lock( containerElement );
+            rewriteUtil.lock( h3 );
+        }
+        // Rewrite the main link
+        rewriteUtil.doLink( wikiInfo, linkElement );
+        // Rewrite the network header
+        const networkHeader = containerElement.querySelector( this.SITE_NETWORK_TITLE_SELECTOR );
+        if ( networkHeader ) {
+            const badgeElement = constructRedirectBadge( {
+                isGoogleMobile: isMobile,
+                allMoved: isTopLevel
+            } );
+            networkHeader.textContent = 'wiki.gg';
+            networkHeader.appendChild( badgeElement );
+            rewriteUtil.lock( networkHeader );
+        }
+        // Rewrite translate link
+        for ( const translate of containerElement.querySelectorAll( this.TRANSLATE_SELECTOR ) ) {
+            rewriteUtil.doLink( wikiInfo, translate );
+        }
+        // Rewrite URL element
+        if ( !isMobile ) {
+            for ( const cite of containerElement.getElementsByTagName( 'cite' ) ) {
+                if ( cite.firstChild.textContent ) {
+                    cite.firstChild.textContent = cite.firstChild.textContent.replace( oldDomain, newDomain );
+                }
+            }
+        } else {
+            const mobileBreadcrumb = containerElement.querySelector( '.sCuL3 > div' );
+            if ( mobileBreadcrumb ) {
+                mobileBreadcrumb.textContent = mobileBreadcrumb.textContent.replace( oldDomain, newDomain );
+            }
+        }
+
+        // Look for "More results from" in this result group and switch them onto wiki.gg
+        for ( const moreResults of containerElement.querySelectorAll( this.MORE_FROM_NETWORK_SELECTOR ) ) {
+            moreResults.href = moreResults.href.replace( 'site:fandom.com', 'site:wiki.gg' )
+                .replace( `site:${wikiInfo.oldId || wikiInfo.id}.fandom.com`, `site:${wikiInfo.id}.wiki.gg` );
+            moreResults.innerText = moreResults.innerText.replace( 'fandom.com', 'wiki.gg' );
+        }
+    }
+}
 
 
 // Set up an observer for dynamically loaded results
@@ -222,8 +212,7 @@ if ( bottomStuff ) {
                             awaitElement(
                                 addedNode,
                                 'div',
-                                results => invokeSearchModule( wikis, rewrite.run.bind( rewrite ), filter.run.bind( filter ),
-                                    results )
+                                results => GoogleSearchModule.invoke( wikis, results )
                             );
                         }
                     }
@@ -236,4 +225,4 @@ if ( bottomStuff ) {
     );
 }
 // Run the initial filtering
-invokeSearchModule( wikis, rewrite.run.bind( rewrite ), filter.run.bind( filter ) );
+GoogleSearchModule.invoke( wikis, results );
